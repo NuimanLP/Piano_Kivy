66c73f10fcd3b5d54c5f9c80dc8648496e845ee3
Pattarapon <peternus.2005@gmail.com>
2024-02-15 00:09:59 +0700

    Add Title Page (Using ScreenManager and Screen)

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 
-class TetrisGame(Widget):
+class TetrisGame(ScreenManager):
+    def __init__(self, *args, **kwargs):
+        super(TetrisGame, self).__init__(*args, **kwargs)
+        self.add_widget(TitleScreen())
+
+
+class TitleScreen(Screen):
     pass
 
 
