8a18b4b8844ea6e5ac8e0a308632a17d9a9f07ca
Chavatik <6610110066@psu.ac.th>
2024-03-11 19:25:22 +0700

    clean

--- main.py
+++ main.py
@@ -7,30 +7,11 @@ from kivy.uix.button import Button
 from kivy.properties import Clock
 
 
-class GameManager(ScreenManager):
-
-    def __init__(self, *args, **kwargs):
-        super(GameManager, self).__init__(*args, **kwargs)
-        self.add_widget(TitleScreen())
-
-    def start_app(self):
-        if self.has_screen("piano"):
-            self.remove_widget(self.get_screen("piano"))
-        self.add_widget(GameScreen())
-        self.current = "piano"
-
-
-class TitleScreen(Screen):
-    pass
-
-
-class GameScreen(Screen):
-    pass
 
 
 class PianoApp(App):
     def build(self):
-        return GameManager()
+        return
 
 
 if __name__ == "__main__":
