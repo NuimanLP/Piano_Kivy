8aae14659390c6d500e3f43af5f8d86fd3630749
Pattarapon <peternus.2005@gmail.com>
2024-03-11 19:40:25 +0700

    Make class Piano and keys

--- main.py
+++ main.py
@@ -6,7 +6,28 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.properties import Clock
 
+Key = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+]
 
+
+class Piano(GridLayout):
+    def __init__(self, **kwargs):
+        super(Piano, self).__init__(**kwargs)
+        self.cols = 10
+        self.make_buttons()
+
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+        self.is_pressed()
 
 
 class PianoApp(App):
