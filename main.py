e51b83e7cc81afbc152eedcd16a2d0497ece6659
Chavatik <6610110066@psu.ac.th>
2024-03-12 07:42:30 +0700

    fix

--- main.py
+++ main.py
@@ -1,58 +1,77 @@
 from kivy.app import App
+from kivy.core.audio import SoundLoader
+from kivy.core.window import Window
+
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-from kivy.core.window import Window
-from kivy.core.audio import SoundLoader
-from kivy.clock import Clock
+from kivy.properties import Clock
 
-# Define key mappings for piano keys to keyboard keys
-Key = ['1', '2', '3', '4', '5', '6', '7', '8']
+Key = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+]
+
 
 class Piano(GridLayout):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 8  # Adjust to fit all piano keys in one or more rows
+        super(Piano, self).__init__(**kwargs)
+        self.cols = 10
         self.make_buttons()
-        self._keyboard = Window.request_keyboard(self._keyboard_closed, self, 'text')
-        if self._keyboard:
-            self._keyboard.bind(on_key_down=self._on_keyboard_down)
 
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+        self.is_pressed()
+
+    # Make Buttons
     def make_buttons(self):
-        # Create more buttons to fulfill the widget requirement
-        for i in range(30):  # Adjusted to create 30 buttons
-            btn = Button(text=str(i+1), size_hint=(0.1, 0.1))
-            btn.bind(on_press=self.callback)
-            self.add_widget(btn)
+        for text in range(1, 9):
+            self.add_widget(Button(text=str(text), size_hint=(0.1, 0.01)))
 
+    # Check to clicked buttons
+    def is_pressed(self):
+        for child in self.children:
+            child.bind(on_press=self.callback)
+
+    # callback for pressing buttons
     def callback(self, instance):
-        index = int(instance.text) - 1
-        self.play_sound(index)
+        self.Sound(int(instance.text) - 1)
 
-    def play_sound(self, index):
-        # Assuming you have 30 WAV files named '0.wav' to '29.wav'
-        sound = SoundLoader.load(f'wav/{index}.wav')
+    # play sounds
+    def Sound(self, index):
+        sound = SoundLoader.load("wav/%d.wav" % index)
         if sound:
             sound.play()
+        return True
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
 
-    def my_callback(self, dt, btn):
-        btn.background_color = (1, 1, 1, 1)
+    # callback for change color of Buttons
+    def my_callback(self, dt):
+        for child in self.children:
+            child.background_color = [1, 1, 1, 1]
 
+    # event when we push keyboard keys
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        # This method maps keyboard keys to piano keys
-        if text in Key:
-            index = Key.index(text)
-            btn = self.children[-(index + 1)]  # Access the corresponding button
-            btn.background_color = (0, 1, 1, 1)  # Change color to indicate press
-            self.play_sound(index)
-            Clock.schedule_once(lambda dt: self.my_callback(dt, btn), 0.1)
+        for index, item in enumerate(Key):
+            if keycode[1] == item:
+                self.children[-index - 1].background_color = (0, 2, 2, 1)
+                self.Sound(index)
+                Clock.schedule_once(self.my_callback, 0.4)
 
-class TestApp(App):
+
+class PianoApp(App):
     def build(self):
         return Piano()
 
-if __name__ == '__main__':
-    TestApp().run()
+
+if __name__ == "__main__":
+    PianoApp().run()
