b0a7d2cd9b4e76ba5a8381932e79331192c95396
Pattarapon <peternus.2005@gmail.com>
2024-03-17 12:05:31 +0700

    Add Screen Manager

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from kivy.core.audio import SoundLoader
 from kivy.clock import Clock
+
+
+class GameManager(ScreenManager):
+  pass
+
+class PianoApp(App):
+   def build(self):
+        return GameManager()
+    
+if __name__ == "__main__":
+    PianoApp().run()
