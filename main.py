a604a4c31d31c3cef79d46705c9a922d093eb091
Pattarapon <peternus.2005@gmail.com>
2024-03-17 12:07:45 +0700

    Function start_app

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ class GameManager(ScreenManager):
     def __init__(self, *args, **kwargs):
         super(GameManager, self).__init__(*args, **kwargs)
         self.add_widget(TitleScreen())
+        
+    def start_app(self):
+        if self.has_screen("piano"):
+            self.remove_widget(self.get_screen("piano"))
+        self.add_widget(GameScreen())
+        self.current = "piano"
 
 class PianoApp(App):
    def build(self):
